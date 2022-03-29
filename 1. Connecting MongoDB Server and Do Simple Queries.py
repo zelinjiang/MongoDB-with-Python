d42cb6304114ee9bf7cb1dbd0aefa5fd44068c6d
zelinjiang <86300572+zelinjiang@users.noreply.github.com>
2022-03-28 21:33:01 -0700

    Update 1. Connecting MongoDB Server and Do Simple Queries.py

--- 1. Connecting MongoDB Server and Do Simple Queries.py
+++ 1. Connecting MongoDB Server and Do Simple Queries.py
@@ -9,12 +9,14 @@ db=client.samples_pokemon
 pokemons=db.samples_pokemon
 
 print('========== First Query ==========')
+# Write a query and return the Pokemons that have candycount equal to the sum of month and date of your birthday
 # My birthday is Jun 2nd, so 06 + 02 = 8
 for pokemon in pokemons.find({"candy_count": {"$gte": 8}}):
     pprint(pokemon['name'])
 
 
 print('========== Second Query ==========')
+# Return the Pokemon that have 'num' equal to the month or date of your birthday
 # My birthday is Jun 2nd, so num == '006' or '002'
 for pokemon in pokemons.find({"$or":[{"num":"006"},{"num":"002"}]}):
     pprint(pokemon['name'])
